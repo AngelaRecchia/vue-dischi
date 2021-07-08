54a12207c27a6d1bca6ca5f9a4776f785ed12076
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-07-08 23:19:36 +0200

    header

---
+++ src/components/Content.vue
@@ -0,0 +1,17 @@
+<template>
+  <main></main>
+</template>
+
+<script>
+export default {
+  name: "Content",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/commons.scss";
+main {
+  height: calc(100vh - 50px);
+  background-color: #1e2d3b;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="w-100 d-flex align-items-center">
+    <img src="../assets/logo.png" alt="logo spotify" class="ms-3" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/commons.scss";
+header {
+  height: 50px;
+  background-color: #2e3a46;
+  img {
+    width: 30px;
+  }
+}
+</style>
